3af6b8d199e0d427ba9c612104eb384cbbb0d882
rational96 <rashadkhan96@gmail.com>
2020-12-06 09:04:46 -0800

    tryin

--- src/main/java/com/mycompany/kbwebapp2/KBServlet.java
+++ src/main/java/com/mycompany/kbwebapp2/KBServlet.java
@@ -1,7 +1,7 @@
 
 package servlet;
 
-import com.mycompany.kbwebapp2.KBTest;
+import com.mycompany.kbwebapp2.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
